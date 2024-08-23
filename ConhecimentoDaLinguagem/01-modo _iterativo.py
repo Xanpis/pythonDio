abb5febce06d88bd69f458cda12079d417609076
Xanpis <wellington.org.com@gmail.com>
2024-08-23 19:58:34 -0300

    up

---
+++ ConhecimentoDaLinguagem/01-modo _iterativo.py
@@ -0,0 +1,21 @@
+
+
+"""
+O python possui um modo iterativo onde você pode rodar códigos diretamente
+posso chamar diretamente so colocando (python3) ou o (python -i nome_do_arquivo.py);
+
+"""
+
+
+"""
+Função (dir()) vazia retora o escopo local atual, Como as bibliotecas instaladas
+com aguento retorna a lista de atributos valida para o objeto, ou seja seus métodos
+
+"""
+
+"""
+Com o help() vazio eu posso buscar ajuda na documentação e com o nome do atributo como método eu posso 
+pesquisar tudo sobre aquele método 
+
+"""
+
---
+++ ConhecimentoDaLinguagem/02-constants_variavel.py
@@ -0,0 +1,15 @@
+
+# Declaração normal 
+name = 'Guilherme'
+idade = 27
+
+# Declarando variares na mesma linha 
+name, idade = ('Fulano',28)
+print(f'Name: { name} Idade:{idade}')
+
+# Boas praticas padrão snake case
+saldo_da_conta = 1200
+
+# Definindo constantes é apenas uma convenção não é uma constante de verdade  
+BRAZIL_STATES = ['Mg','Sc','Sp']
+
