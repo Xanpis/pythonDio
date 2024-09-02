c8f26161e8cab209570e392f886e68618ac85fa8
Xanpis <wellington.org.com@gmail.com>
2024-09-02 16:33:56 -0300

    up

---
+++ test.py
@@ -0,0 +1,32 @@
+
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    op = input(menu)
+    
+    # Depositar
+    if op == "d":
+        pass
+            
+    # Sacar        
+    elif op == "s":
+        pass
+    
+    elif op == "e":
+        pass
+    
+    else:
+        print("Opção invalida")
