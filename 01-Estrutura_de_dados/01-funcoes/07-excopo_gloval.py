df03c05a955115a50992565fbbe1beeb6ea5c758
Xanpis <wellington.org.com@gmail.com>
2024-09-03 13:47:53 -0300

    up

--- 01-Estrutura_de_dados/01-funcoes/07-excopo_gloval.py
+++ 01-Estrutura_de_dados/01-funcoes/07-excopo_gloval.py
@@ -6,10 +6,13 @@ apenas se precisar no condigo
 
 salario = 2000
 
-def salario_bonus(bonus,lista):
+def salario_bonus(bonus, lista_test:list):
     global salario
     salario += bonus
-    lista.append(2)
+    # eu tambem estou mudando a variavel (lista) por referencia 
+    lista_test.append(45)
+    # É a mesma coisa que isso 
+    lista.append(70)
     return salario
     
 lista = [1]
@@ -20,11 +23,11 @@ print(salario_bonus(200, lista))
 print(lista)
 
 # Maneira correta
-def salario_bonus2 (bonus,lista):
+def salario_bonus2 (bonus,lis:list):
     global salario
 
     # Fazendo uma copia da lista
-    lista_aux = lista.copy()
+    lista_aux = lis.copy()
     lista_aux.append(5)
     print(f"lista_aux:{lista_aux}")
     
