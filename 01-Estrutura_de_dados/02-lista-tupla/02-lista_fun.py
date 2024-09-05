a000994a264d8eee467d7a95827dd1d4e4b76e0c
Xanpis <wellington.org.com@gmail.com>
2024-09-05 13:35:13 -0300

    up

--- 01-Estrutura_de_dados/02-lista-tupla/02-lista_fun.py
+++ 01-Estrutura_de_dados/02-lista-tupla/02-lista_fun.py
@@ -1,4 +1,7 @@
 
+from traceback import print_tb
+
+
 livro = [1,"Python","Way",30,40,50,30,"Way"]
 
 # copiando lista 
@@ -9,7 +12,46 @@ print(id(lista), id(livro))
 l1 = lista.count("Way")
 print(l1)
 
+# vendo o tamanho da lista 
+l1 = len(lista)
+print(l1)
+
 # Colocando uma lista dentro de outra lista  ela não remove valores duplicados
 lista.extend([50,'python'])
 print(lista)
 
+# Encontrando o objeto de primeira ocorrencia 
+l2 = lista.index(30)
+print(lista[3])
+print(l2)
+
+# Tirando o ultimo elemento da pilha ou com o indice do elemento
+lista.pop()
+print(lista) 
+lista.pop(2)
+print(lista)
+
+# Removendo o objeto sem o indice, remove a primeira ocorrencia 
+lista.remove(30)
+print(lista)
+
+# Colocando lista ao Contrario
+lista.reverse()
+print(lista)
+
+# Ordenando a lista com sort
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort()  # ["c", "csharp", "java", "js", "python"]
+print(linguagens)
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(reverse=True)  # ["python", "js", "java", "csharp", "c"]
+print(linguagens)
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(key=lambda x: len(x))  # ["c", "js", "java", "python", "csharp"]
+print(linguagens)
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(key=lambda x: len(x), reverse=True)  # ["python", "csharp", "java", "js", "c"]
+print(linguagens)
