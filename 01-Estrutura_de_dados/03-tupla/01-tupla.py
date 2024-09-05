45141faa4ae487ddb083c30a6668118ea0d5b729
Xanpis <wellington.org.com@gmail.com>
2024-09-05 17:28:03 -0300

    up

---
+++ 01-Estrutura_de_dados/03-tupla/01-tupla.py
@@ -0,0 +1,11 @@
+# Diferenca de tuple e lista: tuple é um objeto imutavel 
+frutas = ("laranja","manga","uva","melao",) # Sempre adicionar uma , no final
+print(frutas)
+
+# Iteravel
+frutas2 = tuple("Python") 
+print(frutas2)
+
+numeros = tuple([1,2,3,4]) # Passando numeros
+print(numeros)
+
