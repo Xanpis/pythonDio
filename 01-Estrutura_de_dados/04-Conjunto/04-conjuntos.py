1a91cb2d9c68eaa346a2e9d77e2c95dcc05eb553
Xanpis <wellington.org.com@gmail.com>
2024-09-06 14:31:57 -0300

    up

---
+++ 01-Estrutura_de_dados/04-Conjunto/04-conjuntos.py
@@ -0,0 +1,19 @@
+# Set um conjunto de objetos que tem elemento unicos
+# O set não suporta indexação buscar o valor na posicao  
+
+# Eliminando valores duplicados de uma lista 
+numeros = [1, 1, 1, 2, 2, 3, 3,]
+uni = set(numeros)
+print(uni)
+
+# Eliminando Duplicados de um iteravel
+uni = set("abacaxi")
+print(sorted(uni))
+
+# Declarando um set 
+sete7 = {"abacaxi","melao","manga","melao",}
+print(sete7)
+
+# como percorre uma lista 
+for chave, valor  in  enumerate(sete7):
+    print(f" {chave} : {valor} ",end= " | ") 
---
+++ 01-Estrutura_de_dados/04-Conjunto/05-metodos_set.py
@@ -0,0 +1,9 @@
+# União de conjunto
+conjunto_a = {1,2,3,}
+conjunto_b = {2,3,4,}
+uniao = conjunto_a.union(conjunto_b)
+print(uniao)
+
+# Interceção no enter conjunto
+intercecao = conjunto_a.intersection(conjunto_b)
+print(intercecao)
