ad040001a311885e2a4f97e73d977fae2ca811ec
Xanpis <wellington.org.com@gmail.com>
2024-09-06 14:58:32 -0300

    up

--- 01-Estrutura_de_dados/04-Conjunto/05-metodos_set.py
+++ 01-Estrutura_de_dados/04-Conjunto/05-metodos_set.py
@@ -2,8 +2,16 @@
 conjunto_a = {1,2,3,}
 conjunto_b = {2,3,4,}
 uniao = conjunto_a.union(conjunto_b)
-print(uniao)
+print("União: ",uniao)
 
 # Interceção no enter conjunto
 intercecao = conjunto_a.intersection(conjunto_b)
-print(intercecao)
+print("Interceção: ",intercecao)
+
+# Tudo que tenho em um conjunto que nao tenho no outro incluindo a interceção 
+tem_nao_tem = conjunto_a.difference(conjunto_b)
+print("Diferente: ",tem_nao_tem)
+
+# Pegando todos os elementos que não estão nà interceção  
+nao_ta_na_intercecao = conjunto_a.symmetric_difference(conjunto_b)
+print("Ñ está na interceção: ",nao_ta_na_intercecao)
