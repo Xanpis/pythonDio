738b309e49c73cd6634e43f9719499d2cff1cb48
Xanpis <wellington.org.com@gmail.com>
2024-09-07 18:17:00 -0300

    up

--- 0000-Exemplogit/04 - Dicionários/03_iterando_dicionarios.py
+++ 0000-Exemplogit/04 - Dicionários/03_iterando_dicionarios.py
@@ -1,14 +1,14 @@
-contatos = {
+contact = {
     "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
     "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
     "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
-    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766", "add":{"nome: "}},
 }
 
-for chave in contatos:
-    print(chave, contatos[chave])
+for chave in contact:
+    print(chave, contact[chave])
 
 print("=" * 100)
 
-for chave, valor in contatos.items():
+for chave, valor in contact.items():
     print(chave, valor)
