f1040d4ab8560a7dd5f568440c5dd6358f6fe9e4
Xanpis <wellington.org.com@gmail.com>
2024-09-10 19:07:03 -0300

    up

--- 0000-Exemplogit/05 - Data e hora/1_datetime.py
+++ 0000-Exemplogit/05 - Data e hora/1_datetime.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime, time
+from datetime import date, datetime, time, timedelta
 
 data = date(2023, 7, 10)
 print(data)
@@ -12,8 +12,10 @@ hora = time(10, 20, 0)
 print(hora)
 
 hora_min_seg = datetime.now()
+temp0 = 1
 hora = hora_min_seg.hour
 min = hora_min_seg.minute
 seg = hora_min_seg.second
+
 print(f"H:{hora} M:{min} S:{seg}")
 
