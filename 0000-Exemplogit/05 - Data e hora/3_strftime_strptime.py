fa9f8d69ddad0b8c521df7aa168dd37af4f456b1
Xanpis <wellington.org.com@gmail.com>
2024-09-10 18:47:46 -0300

    up

--- 0000-Exemplogit/05 - Data e hora/3_strftime_strptime.py
+++ 0000-Exemplogit/05 - Data e hora/3_strftime_strptime.py
@@ -8,7 +8,6 @@ mascara_en = "%Y-%m-%d %H:%M"
 
 print(data_hora_atual.strftime(mascara_ptbr))
 
-
 data_convertida = datetime.strptime(data_hora_str, mascara_en)
 print(data_convertida)
 print(type(data_convertida))
