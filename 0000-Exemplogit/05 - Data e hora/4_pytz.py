11c53449e05f3b403cf55d1f736b23f7c6349d78
Xanpis <wellington.org.com@gmail.com>
2024-09-09 21:02:21 -0300

    up

--- 0000-Exemplogit/05 - Data e hora/4_pytz.py
+++ 0000-Exemplogit/05 - Data e hora/4_pytz.py
@@ -1,10 +1,8 @@
 from datetime import date, datetime
 import pytz 
 
-
 data = datetime.now(pytz.timezone("Europe/Oslo"))
 data2 = datetime.now(pytz.timezone("America/Sao_Paulo"))
-
 
 print(data)
 print(data2)
