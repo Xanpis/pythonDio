d81aae74dd2a13f7aee951ddb834f1262f150f77
Xanpis <wellington.org.com@gmail.com>
2024-09-17 18:02:58 -0300

    up

---
+++ 03 - Programação Orientada a Objetos/04 - Herança/02-heranca_mutipla_kw.py
@@ -0,0 +1,30 @@
+class Veiculo:
+    def __init__(self, cor):
+        self.cor = cor
+    def __str__(self):
+            return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
+
+class Carro(Veiculo):
+    def __init__(self,cor,lugares):
+            super().__init__(cor)
+            self.lugares = lugares
+
+            
+class Caminhao(Veiculo):
+    def __init__(self,cor,capacidade_de_carga):
+        super().__init__(cor)
+        self.capacidade_de_carga = capacidade_de_carga
+
+
+class car_4x4(Caminhao,Carro):
+    pass
+
+
+"""
+Quando eu instício meu objeto o python não intende para qual construtor eu devo passar o segundo argumento
+para isso eu tenho o **kw, quando eu adiciono ele no construtor,
+eu tenho que passar no parâmetro do objeto instanciado chave e valor
+
+"""   
+carro_4x4 = car_4x4 (cor='verde', lugares= 4, capacidade_de_carga = "200kg" )
