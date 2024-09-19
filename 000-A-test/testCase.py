e6f44a2a9463585e702bb732111e027fbaba9b6a
Xanpis <wellington.org.com@gmail.com>
2024-09-18 21:45:29 -0300

    up

--- 000-A-test/testCase.py
+++ 000-A-test/testCase.py
@@ -4,18 +4,4 @@ class Veiculo:
     def __str__(self):
             return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
 
-
-class Carro(Veiculo):
-    def __init__(self,cor,lugares):
-            super().__init__(cor)
-            self.lugares = lugares
-
-
-
-"""
-Quando eu instício meu objeto o python não intende para qual construtor eu devo passar o segundo argumento
-para isso eu tenho o **kw, quando eu adiciono ele no construtor,
-eu tenho que passar no parâmetro do objeto instanciado chave e valor
-
-"""   
-carro_4x4 = car_4x4 (cor='verde', lugares= 4, capacidade_de_carga = "200kg" )
+vaca = Veiculo('verde')
