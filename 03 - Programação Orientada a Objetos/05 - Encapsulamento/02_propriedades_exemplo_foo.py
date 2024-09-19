777833ab6f4589de7f8853a8cae1bfdafae2c3b0
Xanpis <wellington.org.com@gmail.com>
2024-09-19 17:56:16 -0300

    up

--- 03 - Programação Orientada a Objetos/05 - Encapsulamento/02_propriedades_exemplo_foo.py
+++ 03 - Programação Orientada a Objetos/05 - Encapsulamento/02_propriedades_exemplo_foo.py
@@ -1,11 +1,13 @@
 class Foo:
     def __init__(self, x=None):
         self._x = x
+        
 
     """
     Com o property eu posso criar atributos gerenciados ou computada ou seja quando precisar 
     modificar sua implementação  interna sem alterar a api publica da classe.
-    Aqui estou pegando x se tiver valor ele retorna o valor, ou 0  
+    A propriedade ela pega um método e transforma ele em uma propriedade  
+    Aqui estou pegando x se tiver valor ele retorna se não retorne  0  
     """
     @property
     def x(self):
@@ -13,17 +15,24 @@ class Foo:
 
     @x.setter
     def x(self, value):
-        _x = self._x or 0
-        _valor = value or 0
-        self._x += value
+        
+        # essa  é uma maneira de trabalhar com porrete
+        # _x = self._x or 0
+        # _valor = value or 0
+        # self._x = _x + _valor
+        
+        # Eu não posso usar o retorno nesse caso porque ele não é um método mas uma propriedade 
+        # Return  self._x += value
+        
+        # Correto
+         self._x += value
 
     @x.deleter
     def x(self):
         self._x = 0
 
 
-foo = Foo(10)
-print(foo.x)
+foo = Foo(6)
 foo.x = 10 # vai se agora 20
 print(foo.x)
 del foo.x
--- 03 - Programação Orientada a Objetos/05 - Encapsulamento/03_propriedades_exemplo_pessoa.py
+++ 03 - Programação Orientada a Objetos/05 - Encapsulamento/03_propriedades_exemplo_pessoa.py
@@ -2,6 +2,12 @@ class Pessoa:
     def __init__(self, nome, ano_nascimento):
         self.nome = nome
         self._ano_nascimento = ano_nascimento
+        
+        
+    # Se eu não tenho regra de NEGOCIO para validar ou modificar, eu não preciso do property   
+    @property
+    def name(self):
+        return self.nome
 
     @property
     def idade(self):
@@ -11,3 +17,4 @@ class Pessoa:
 
 pessoa = Pessoa("Guilherme", 1994)
 print(f"Nome: {pessoa.nome} \tIdade: {pessoa.idade}")
+    
