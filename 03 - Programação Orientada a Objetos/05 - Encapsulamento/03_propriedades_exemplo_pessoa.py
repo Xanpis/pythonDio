9a0cb709a1cfa8c58d5c20b234b8ad68a20b6b2c
Xanpis <wellington.org.com@gmail.com>
2024-09-21 15:09:59 -0300

    up

--- 03 - Programação Orientada a Objetos/05 - Encapsulamento/03_propriedades_exemplo_pessoa.py
+++ 03 - Programação Orientada a Objetos/05 - Encapsulamento/03_propriedades_exemplo_pessoa.py
@@ -4,11 +4,13 @@ class Pessoa:
         self._ano_nascimento = ano_nascimento
         
         
-    # Se eu não tenho regra de NEGOCIO para validar ou modificar, eu não preciso do property   
+    # NOTE : Se eu não tenho regra de NEGOCIO para validar ou modificar, eu não preciso do property 
+    # é mas a carra do python chamas a atributo diretamente quando for publico...
     @property
     def name(self):
         return self.nome
 
+    # A qui eu preciso porque estou validando algo privado
     @property
     def idade(self):
         _ano_atual = 2022
