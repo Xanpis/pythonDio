cd10d386558a506722c2de4da12ecac547463731
Xanpis <wellington.org.com@gmail.com>
2024-09-22 09:59:49 -0300

    up

--- 000-A-test/test.py
+++ 000-A-test/test.py
@@ -1,43 +1,10 @@
-
-# menu = """
-
-# [d] Depositar
-# [s] Sacar
-# [e] Extrato
-# [q] Sair
-
-# => """
-
-# saldo = 0
-# limite = 500
-# extrato = ""
-# numero_saques = 0
-# LIMITE_SAQUES = 3
-
-# while True:
-#     op = input(menu)
-    
-#     # Depositar
-#     if op == "d":
-#         pass
-            
-#     # Sacar        
-#     elif op == "s":
-#         pass
-    
-#     elif op == "e":
-#         pass
-    
-#     else:
-#         print("Opção invalida")
-        
         
 
 
 # Components of another_year add up to exactly 365 days
 # from datetime import timedelta
 # year = timedelta(days=365)
-# another_year = timedelta(weeks=40, days=84, hours=23,minutes=50, seconds=600)
+# another_year = timedelta(weeks=40, days=84, hours=23, minutes=50, seconds=600)
 # resultado = year.total_seconds() == another_year.total_seconds()
 # resultado2 = year == another_year
 
