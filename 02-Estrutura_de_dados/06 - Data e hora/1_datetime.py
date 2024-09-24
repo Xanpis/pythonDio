6dd8ae30bf82df96fdd3228ed4074de3a32cefc4
Xanpis <wellington.org.com@gmail.com>
2024-09-23 22:30:59 -0300

    up

--- 02-Estrutura_de_dados/06 - Data e hora/1_datetime.py
+++ 02-Estrutura_de_dados/06 - Data e hora/1_datetime.py
@@ -1,16 +1,20 @@
 from datetime import date, datetime, time, timedelta
 
+# Na função data()  Eu passo um inteiro nos parâmetros e ela converte para OBJ tipo data,
 data = date(2023, 7, 10)
 print(data)
 print(date.today())
 
+# Retorna um obj  data, hora, segundos, e minutos
 data_hora = datetime(2023, 7, 10,)
 print(data_hora)
 print(datetime.today())
 
+# Retorna um obj do tipo hora mas só o que for passado por parâmetro, não retorna o atual  
 hora = time(10, 20, 0)  
 print(hora)
 
+# Pegando os horários atuais 
 hora_min_seg = datetime.now()
 hora = hora_min_seg.hour
 min = hora_min_seg.minute
