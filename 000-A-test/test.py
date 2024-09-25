260b62a515c60664d1a26cf6dc6e4ae8274e3c16
Xanpis <wellington.org.com@gmail.com>
2024-09-25 19:18:27 -0300

    up

--- 000-A-test/test.py
+++ 000-A-test/test.py
@@ -8,29 +8,6 @@
 
 # print(resultado,"---",resultado2)  
 
-# carro = {}
-# while True:
-#     print('1 - Cadastra') 
-#     print('2 - lista') 
-#     print('0 - Sair')
-#     op = input('Opção: ')
-   
-#     if op == '1':
-#         dono =  input('Dono: ')
-#         cor =  input('Cor: ')
-#         placa =  input('Placa: ')
-
-#         car = Carro(dono, cor, placa)
-#         carro[car.dono] = {'cor':car.cor , 'placa': car.placa} 
-        
-#     elif op == '2':    
-#         for a,b in carro.items():
-#             print(f"Dono: {a} | Cor: {b['cor']} | Placa: {b['placa']}")
-        
-#     elif op == '0':
-#         break
-    
-
 
 class Carro:
     def __init__(self,dono,cor,placa) -> None:
@@ -41,7 +18,7 @@ class Carro:
     def __str__(self):
         return f"{self.__class__.__name__} : {','.join( [f" {i}: {r}" for i , r  in self.__dict__.items()] )}"
     
-carro = []
+carro = {}
 while True:
     print('1 - Cadastra') 
     print('2 - lista') 
@@ -54,11 +31,12 @@ while True:
         placa =  input('Placa: ')
 
         car = Carro(dono, cor, placa)
-        carro.append(car)
+        carro[car.dono] = {'cor':car.cor , 'placa': car.placa} 
         
     elif op == '2':    
-        for i in carro:
-            print(i)
+        print('######### LIsta De Carros ##########')
+        for a,b in carro.items():
+            print(f"Dono: {a} | Cor: {b['cor']} | Placa: {b['placa']}")
         
     elif op == '0':
         break
