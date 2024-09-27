1dedbb52bc367cba0f33d7d2e990489abe8f20db
Xanpis <wellington.org.com@gmail.com>
2024-09-27 15:32:41 -0300

    up

---
+++ 03 - Programação Orientada a Objetos/08 - Métodos de classe e métodos estáticos/01-metodo_de_class_errado.py
@@ -0,0 +1,22 @@
+class Pessoa:
+    def __init__(self, nome = None, idade = None):
+        self.nome = nome
+        self.idade = idade
+    
+    
+    def criar_de_data_nascimento(self, ano, mes, dia, nome):
+        idade = 2022 - ano
+        # Retornando para a instancia da minha classe
+        return Pessoa (nome, idade)
+    
+    # Jeito correto, assim eu instancio minha classe apenas um vez, referenciando minha classe com o classmethod
+    # @classmethod
+    # def criar_de_data_nascimento(cls, ano, mes, dia, nome):
+    #     idade = 2022 - ano
+    #     return cls(nome, idade)
+    
+# Quando eu tenho esse comportamento de instanciar minha minha casse duas veze para 
+# Mudar seu comportamento eu uso o classmethod    
+p = Pessoa().criar_de_data_nascimento(1997,3,22,"Furão")
+
+print(p.nome, p.idade)
---
+++ 03 - Programação Orientada a Objetos/08 - Métodos de classe e métodos estáticos/02_metodos_classe_estatico.py
@@ -0,0 +1,31 @@
+
+"""
+Todos são Método veiculados a classe e não a instancia da classe.
+Quando  usar métodos de classe = Quando eu quero criar métodos de fabrica 
+Quando  usar métodos estático = Geralmente usado para criar funções utilitária
+
+"""
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        
+    # Diferença è que  no método de classe primeiro parâmetro aponta para classe o método estático não.
+    # O método de classe pode acessar ou modificar o estado da classe, o estático não.. por que não ? 
+    # porque ele não tem a referencia para fazer esse acesso 
+    # o class retorna um nova estancia da classe pessoa
+    
+    @classmethod
+    def criar_de_data_nascimento(cls, ano, mes, dia, nome):
+        idade = 2022 - ano
+        return cls(nome, idade)
+
+    @staticmethod
+    def e_maior_idade(idade):
+        return idade >= 18
+
+p = Pessoa.criar_de_data_nascimento(1994, 3, 21, "Guilherme")
+print(p.nome, p.idade)
+
+print(Pessoa.e_maior_idade(18))
+print(Pessoa.e_maior_idade(8))
