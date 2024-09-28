62868c54c9a3c17fe1c0efd98b78d44952a16620
Xanpis <wellington.org.com@gmail.com>
2024-09-28 19:43:17 -0300

    up

--- 000-A-test/test-01.py
+++ 000-A-test/test-01.py
@@ -1,6 +1,33 @@
+import textwrap
+# entrada = input()
+# erro = entrada.split(',')
+# vendas = list(map(int,[i for i in erro if i.strip()]))
 
-entrada = input()
-erro = entrada.split(',')
-vendas = list(map(int,[i for i in erro if i.strip()]))
+# print(vendas)
 
-print(vendas)
+class Pessoa:
+    def __init__(self,nome,idade):
+        self.nome = nome
+        self.idade = idade
+        
+    def __str__(self):
+        return f"""
+                nome: {self.nome}
+                idade: {self.idade}
+            """
+    
+
+def add():
+    nome = input('Nome: ')
+    idade = input('idade: ')
+    conta = Pessoa(nome, idade)
+    array.append(conta)
+        
+array = []
+
+add()
+
+for i in array:
+    print(i)
+
+print(array)
