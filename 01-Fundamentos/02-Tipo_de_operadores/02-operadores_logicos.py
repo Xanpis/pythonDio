4414c5247a192104c30be80423f50470c0c686b9
Xanpis <wellington.org.com@gmail.com>
2024-10-06 17:19:52 -0300

    up

--- 01-Fundamentos/02-Tipo_de_operadores/02-operadores_logicos.py
+++ 01-Fundamentos/02-Tipo_de_operadores/02-operadores_logicos.py
@@ -2,13 +2,13 @@ saldo = 1000
 saque = 200
 limite = 100
 
-# verdade e verdade = verdade
+# verdade e verdade == verdade
 if saldo >= saque and saque <= limite:
     print("Sacando Dinheiro")
 else:
     print("Valor Incorreto")
  
-# verdade ou falso = falso   
+# verdade ou falso == falso   
 if saldo >= saque or saque <= limite:
     print("Sacando Dinheiro")
 else:
