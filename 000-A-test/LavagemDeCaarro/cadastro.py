1be4937bb6de7c529d3432fb5e64a4addabd5501
Xanpis <wellington.org.com@gmail.com>
2024-10-08 15:42:19 -0300

    up

--- 000-A-test/LavagemDeCaarro/cadastro.py
+++ 000-A-test/LavagemDeCaarro/cadastro.py
@@ -1,50 +1,49 @@
 
 
-# pegando os dados dos usuários e seu carro 
+# Dados relacionados com o usuário e o carro 
 class User: 
     def __init__(self,name,placa) -> None:
         self.name = name
         self._placa = placa
         
+    def __str__(self) -> str:
+        return f"Dono do carro = {" | ".join([f"{chave}: {valor}" for chave,valor in self.__dict__.items()]).title()}"
+    
     @property
     def placa(self):
-        return self._placa
+        return self._placa or 0
     
     @placa.setter
     def placa(self,placa):
         try:
             if placa == 1:
-                self._placa 
-                    
-        except:
-            print(f'@@@ placa erro @@@ {self.name}')   
+                self._placa                          
+        except Exception as e:
+            print(f'@@@ placa erro {e} @@@ {self.name} ')   
             
-    def __str__(self) -> str:
-        return f"Dono do carro = {" | ".join([f"{chave}: {valor}" for chave,valor in self.__dict__.items()]).title()}"
 
     
-# Manipulando os dados   
+# funções para manipulação da classe User   
 class Cadastros(): 
     def __init__(self) -> None:
-        self._cadastrados = [] 
-        
+        self._cadastrados = []   
     
     def add(self,obj):
         self._cadastrados.append(obj)
     
     def buscarUser(self,placa):
-        placaUser = "".join([f"{chave}" for chave in self._cadastrados if chave._placa == placa])   
-           
+        placaUser = "".join([f"{chave}" for chave in self._cadastrados if chave._placa == placa])    
         return placaUser or False
   
     
 ca = Cadastros()
-carro1 = User('joaninha', 123)
-ca.add(carro1)
 
-carro1 = User('joão', 125)
-ca.add(carro1)
-carro1.placa = 2
+carro = User('joaninha', 123)
+ca.add(carro)
 
-print(carro1)
+carro = User('joão', 125)
+ca.add(carro)
+carro.placa = 2
+
+print(carro)
 
