357be3c44441787d7f7173d0e71cdec9e505feab
Xanpis <wellington.org.com@gmail.com>
2024-10-08 15:28:50 -0300

    up

--- 000-A-test/LavagemDeCaarro/cadastro.py
+++ 000-A-test/LavagemDeCaarro/cadastro.py
@@ -1,29 +1,39 @@
-from datetime import datetime
-mascara_hora = "%H:%M"
-hora = datetime.now()
 
 
 # pegando os dados dos usuários e seu carro 
 class User: 
     def __init__(self,name,placa) -> None:
         self.name = name
-        self.placa = placa
-        self.data = datetime.now().date()
-        self.hora = hora.time().strftime(mascara_hora)
+        self._placa = placa
         
+    @property
+    def placa(self):
+        return self._placa
+    
+    @placa.setter
+    def placa(self,placa):
+        try:
+            if placa == 1:
+                self._placa 
+                    
+        except:
+            print(f'@@@ placa erro @@@ {self.name}')   
+            
     def __str__(self) -> str:
-        return f"{" | ".join([f"{chave}: {valor}" for chave,valor in self.__dict__.items()]).title()}"
+        return f"Dono do carro = {" | ".join([f"{chave}: {valor}" for chave,valor in self.__dict__.items()]).title()}"
+
     
 # Manipulando os dados   
 class Cadastros(): 
     def __init__(self) -> None:
-        self.cadastrados = [] 
+        self._cadastrados = [] 
+        
     
     def add(self,obj):
-        self.cadastrados.append(obj)
+        self._cadastrados.append(obj)
     
     def buscarUser(self,placa):
-        placaUser = "".join([f"{chave}" for chave in self.cadastrados if chave.placa == placa])   
+        placaUser = "".join([f"{chave}" for chave in self._cadastrados if chave._placa == placa])   
            
         return placaUser or False
   
@@ -32,10 +42,9 @@ ca = Cadastros()
 carro1 = User('joaninha', 123)
 ca.add(carro1)
 
-carro1 = User('joaninha', 125)
+carro1 = User('joão', 125)
 ca.add(carro1)
+carro1.placa = 2
 
-print(ca.buscarUser(123))
+print(carro1)
 
-p = "fasdf"
-print(p.title())
