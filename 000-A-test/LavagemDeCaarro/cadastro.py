8bf2a689758de77898217994a3d89f08d0535318
Xanpis <wellington.org.com@gmail.com>
2024-10-13 20:34:09 -0300

    up

--- 000-A-test/LavagemDeCaarro/cadastro.py
+++ 000-A-test/LavagemDeCaarro/cadastro.py
@@ -1,23 +1,31 @@
-
-
-car,user = [],[]
+choses = f"""
+[1] Cadastra Usuário
+[2] Lista Usuário
+[] Trocar O Nome Do Usuário
+[] Deletar Usuário
+[] Cadastra Carro
+[] Cadastra Caminhão
+[] Buscar Veiculo
+[] Deletar veiculo
+[] Lavar Veiculo
+[] Retira Veiculo da Lavagem
+[0] Sair
+"""
 
 # Classe para usuário 
 class User: 
-    try:
-        def __init__(self, name, idade) -> None:
-            if not self.validar_nome(name):
-                raise ValueError("Nome deve ser uma string e ser maior que 3 caracteres.")
-            if not self.validar_idade(idade):
-                raise ValueError("Idade deve ser um número inteiro positivo. Mair que (18) e Menor que (70) ")
-            
-            self.name = name
-            self.idade = idade
-    except:
-        print('Nome ou idade incorreto')
+    def __init__(self, name, idade) -> None:
+        if not self.validar_nome(name):
+            raise ValueError("@@ Nome deve ser uma string e ser maior que 3 caracteres.@@")
+        if not self.validar_idade(idade):
+            raise ValueError("@@ Idade deve ser um número inteiro positivo. Mair que (18) e Menor que (70) @@")
+        
+        self.name = name
+        self.idade = idade
+  
         
     def __str__(self) -> str:
-        return f"Dono = {' | '.join([f'{chave}: {valor}' for chave, valor in self.__dict__.items()])}"
+        return f"User = {' | '.join([f'{chave}: {valor}' for chave, valor in self.__dict__.items()])}"
 
     @staticmethod
     def validar_nome(nome):
@@ -27,46 +35,21 @@ class User:
     def validar_idade(idade):
         return isinstance(idade, int) and idade >= 18 and idade <= 70
     
-    def buscando_user(self,nome:str):
-        if not user:
-            return '@@@ Erro Lista de Usuário vazia @@@'
-        else:  
-            for i in user:
-                if i.name == nome:
-                    return ValueError(i) 
-                else:
-                    raise ValueError("@@@ Erro nome não localizado @@@")  
                       
-
             
 # Classe para cadastra o carro recebendo um objeto do tipo Usuário  
-class Car: 
-
-    def __init__(self,user:User, cor , placa) -> None:
+class Car(): 
+    def __init__(self, cor, placa):
         
         if not self.validar_placa(placa):
             raise ValueError("A placa dever ter exatamente 4 números positivos")
         
-        self.user = user
         self.cor = cor
         self._placa = placa 
          
     def __str__(self) -> str:
-            return f"{self.user} | Veiculo: {self.__class__.__name__} | Cor: {self.cor} | Placa: {self._placa}"
+            return f"| Veiculo: {self.__class__.__name__} | Cor: {self.cor} | Placa: {self._placa}"
          
-    # encapsulamento
-    @property
-    def placa(self):
-        return self._placa or 0
-    
-    @placa.setter
-    def placa(self, placa:int):
-        try:
-            if isinstance(placa,int()):
-                self._placa   # olhar porque o exception não esta funcionando                      
-        except Exception as e:
-            print(f'@@@ placa erro {e} @@@ {self.name} ')
-               
             
     # Método para validar classe sem precisar instanciar a classe                    
     @staticmethod
@@ -78,14 +61,14 @@ class Car:
     def buscar_id_placa(self,placa:int):
         placaUser = "".join([f"{chave}" for chave in car if chave.placa == placa])    
         return placaUser or False  
+
               
-      
+# Class caminhão      
 class Trunk(Car):
-    def __init__(self, user, cor, placa):
-        super().__init__(user, cor, placa)
+    def __init__(self, cor, placa):
+        super().__init__(cor, placa)
 
-
-            
+           
 # métodos de manipulação 
 class Lavar_cadastrados:
     def __init__(self, user, placa, tempo) -> None:
@@ -111,28 +94,53 @@ class Lavar_cadastrados:
                 return  
     
     
-try:
-    use = User('porch',28)
-    print(use)
-    user.append(use)
-except ValueError as e:
-    print(e)
+car,user = [],[]
+# while True:
+#     op = input(f"{choses}> " )
+
+#     if op == '1':
+#         nome = input("Nome: ")
+#         idade =int(input("Idade: "))
+#         try:
+#             use = User(nome,idade)
+#             user.append(use)
+#             print("## Usuário Cadastrado ##")
+#         except ValueError as e:
+#             print(e)
+
+#     if op == '2':
+#         if not user:
+#             print("@@@ Não tem Usuário cadastrado @@@")
+#         else:
+#             for i in user:
+#                 print(50* "-")
+#                 print(i)
+
+#     if op == '0':
+#         break
+
+us = User('guio',34)
+t = Car('verde', '1345')
+print(us)
+
+
+
 
     
-try:
-    print(use.buscando_user("mu"))
-except ValueError as e:
-    print(e)
 
 
-try:
-    tru = Trunk(use,'verde','1234')
-    print(tru)
-except ValueError as e:
-    print(e)
+
+
+
     
-la = Lavar_cadastrados(tru,'1234',50)
-print(la)
-    
+# try:
+#     print(use.buscando_user("mu"))
+# except ValueError as e:
+#     print(e)
 
 
+# try:
+#     tru = Trunk(use,'verde','1234')
+#     print(tru)
+# except ValueError as e:
+#     print(e)
