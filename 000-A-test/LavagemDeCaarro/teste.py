51f3715e524233b73b15a0b5b1374337c9bdf5cb
Xanpis <wellington.org.com@gmail.com>
2024-10-16 19:31:25 -0300

    up

--- 000-A-test/LavagemDeCaarro/teste.py
+++ 000-A-test/LavagemDeCaarro/teste.py
@@ -1,9 +1,7 @@
 
 array = ['nao','sim','nao','sim','ok','bem']
 une = []
-
-[une.append(item) for item in array  if not item in une ]
-        
+[une.append(item) for item in array  if not item in une ]  
 une.sort()
 print(une)
 
@@ -15,5 +13,4 @@ print(lista_sem_duplicatas)  # A ordem pode não ser preservada
 
 lista_com_duplicatas = ["maçã", "banana", "maçã", "laranja", "banana"]
 lista_sem_duplicatas = list(dict.fromkeys(lista_com_duplicatas))
-
 print(lista_sem_duplicatas)
