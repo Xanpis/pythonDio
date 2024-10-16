ef6600bacc8325ffd571f1a86f699deb7436db4a
Xanpis <wellington.org.com@gmail.com>
2024-10-16 19:29:52 -0300

    up

--- 000-A-test/LavagemDeCaarro/teste.py
+++ 000-A-test/LavagemDeCaarro/teste.py
@@ -1,9 +1,19 @@
-class Car:
-    def __init__(self,name) -> None:
-        self.name = name
-    
-    def __str__(self) -> str:
-        return __class__.__name__
+array = ['nao','sim','nao','sim','ok','bem']
+une = []
 
-u = Car('Fulano')
-print(u)
+[une.append(item) for item in array  if not item in une ]
+        
+une.sort()
+print(une)
+
+
+lista_com_duplicatas = ["maçã", "banana", "maçã", "laranja", "banana"]
+lista_sem_duplicatas = list(set(lista_com_duplicatas))
+
+print(lista_sem_duplicatas)  # A ordem pode não ser preservada
+
+
+lista_com_duplicatas = ["maçã", "banana", "maçã", "laranja", "banana"]
+lista_sem_duplicatas = list(dict.fromkeys(lista_com_duplicatas))
+
+print(lista_sem_duplicatas)
