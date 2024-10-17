5ada551505189ff7afcee6f821d822719bb57610
Xanpis <wellington.org.com@gmail.com>
2024-10-17 20:43:52 -0300

    up

--- 000-A-test/test.py
+++ 000-A-test/test.py
@@ -8,46 +8,7 @@
 
 # print(resultado,"---",resultado2)  
 
-show = """
-    1 - Cadastra
-    2 - lista
-    0 - Sair
-"""
 
-# Variável Global
-carro = []
-
-class Carro:
-    def __init__(self,dono,cor,placa) -> None:
-        self.dono = dono
-        self.cor = cor
-        self.placa = placa
-        
-    def __str__(self):
-        return f"{self.__class__.__name__} : {','.join( [f" {i}: {r}" for i , r  in self.__dict__.items()] )}"
-    
-    
-while True:
-    print(show)
-    op = input('Opção: ')
-   
-    if op == '1':
-        
-        dono =  input('Dono: ')
-        cor =  input('Cor: ')
-        placa =  input('Placa: ')
-
-        car = Carro(dono, cor, placa)
-        carro.append(car)
-        
-    elif op == '2':    
-        print('######### LIsta De Carros ##########')
-        for i in carro:
-            print(f'Dono: {i.dono} | Cor: {i.cor} | placa: {i.placa}')
-            print('-'*50)
-        
-    elif op == '0':
-        break
     
     
 
