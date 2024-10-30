9bf76dcdede492efcb52446581c3fadad67a9ee4
Xanpis <wellington.org.com@gmail.com>
2024-10-30 20:41:30 -0300

    up

--- 01-Fundamentos/04-Dominando_string/03-fatiamento.py
+++ 01-Fundamentos/04-Dominando_string/03-fatiamento.py
@@ -8,7 +8,7 @@ nome = "Manga "
 # o 4 vai até o g 4-1 = 3 de 0 á 3 = 4
 print(nome[:4])
 
-# pega do 4 uma casa a frente ou seja comeca com (a) 
+# pega do 4 uma casa a frente ou seja começa com (a) 
 print(nome[4:])
 
 #  espelhando 
