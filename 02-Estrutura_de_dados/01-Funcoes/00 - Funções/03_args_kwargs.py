85f0320571ee255e1ae679119dfc1695f85e2ffb
Xanpis <wellington.org.com@gmail.com>
2024-10-31 19:54:08 -0300

    up

--- 02-Estrutura_de_dados/01-Funcoes/00 - Funções/03_args_kwargs.py
+++ 02-Estrutura_de_dados/01-Funcoes/00 - Funções/03_args_kwargs.py
@@ -1,8 +1,9 @@
-def exibir_poema(data_extenso, *args, **kwarg):
+def exibir_poema(data_extenso, *args, **kwargs):
     texto = "\n".join(args)
     meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
     mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
     print(mensagem)
+  
 
 
 exibir_poema(
