45fea7f932e685184e787620d71fb9a64e511891
Xanpis <wellington.org.com@gmail.com>
2024-11-01 22:08:12 -0300

    up

--- 02-Estrutura_de_dados/01-Funcoes/00 - Funções/05_parametros_somente_por_nome.py
+++ 02-Estrutura_de_dados/01-Funcoes/00 - Funções/05_parametros_somente_por_nome.py
@@ -1,5 +1,9 @@
 def criar_carro(modelo, ano, placa, /, *, marca, motor, combustivel):
     print(modelo, ano, placa, marca, motor, combustivel)
+    
+    
+    # / Por posição
+    # * Nomeado  
 
 
 # criar_carro("Palio", 1999, "ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")
