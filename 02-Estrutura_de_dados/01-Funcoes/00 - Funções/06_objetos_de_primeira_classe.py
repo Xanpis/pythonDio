3253ab8757ae0cb1ec9f0c34861559770e4f3f62
Xanpis <wellington.org.com@gmail.com>
2024-11-19 22:41:54 -0300

    up

--- 02-Estrutura_de_dados/01-Funcoes/00 - Funções/06_objetos_de_primeira_classe.py
+++ 02-Estrutura_de_dados/01-Funcoes/00 - Funções/06_objetos_de_primeira_classe.py
@@ -6,5 +6,4 @@ def exibir_resultado(a, b, funcao):
     resultado = funcao(a, b)
     print(f"O resultado da operação {a} + {b} = {resultado}")
     
-
 exibir_resultado(10, 10, somar)  # O resultado da operação 10 + 10 = 20
